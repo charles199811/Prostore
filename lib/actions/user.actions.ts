9b07d48fced3e9d66c980d38c6e134ff2cef7f20
Charles Rodrigo <charlesrodrio@gmail.com>
2025-07-29 15:44:06 +0100

    add validators and set the user actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { signInFormSchema } from "../validators";
+import { signIn, signOut } from "@/auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+//Sign in the user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+    try {
+        const user = signInFormSchema.parse({
+            email: formData.get('email'),
+            password: formData.get('password')
+        });
+
+        await signIn('credentials', user);
+
+        return { success: true, massage: 'Signed in success'}
+    } catch (error) { 
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        return { success: false, message: 'Invalid email or password'}
+    }
+}
+
+//sign user out
+export async function signOutUser() {
+    await signOut();
+}
