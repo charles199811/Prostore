bea35350aad5d99e315f4e80e4926f2412c80670
Charles Rodrigo <charlesrodrio@gmail.com>
2025-07-29 14:44:54 +0100

    user data added and set the route and authentications

--- auth.ts
+++ auth.ts
@@ -1,67 +1,71 @@
-// import NextAuth from 'next-auth';
-// import { PrismaAdapter } from '@auth/prisma-adapter';
-// import { prisma } from '@/db/prisma';
-// import CredentialsProvider from 'next-auth/providers/credentials';
-// import { compareSync } from 'bcrypt-ts-edge';
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/db/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compareSync } from "bcrypt-ts-edge";
+import type { NextAuthConfig } from "next-auth";
 
-// export const config = {
-//     pages: {
-//         signIn: '/sign-in',
-//         error: '/sign-in'
-//     },
-//     sesiion: {
-//         stratergy: 'jwt',
-//         maxAge: 30 * 24 * 60 * 60, // 30 days
-//     },
-//     adapter: PrismaAdapter(prisma),
-//     providers: [
-//         CredentialsProvider({
-//             credentials: {
-//                 email: { type: 'email' },
-//                 password: { type: 'password' }
-//             },
+export const config = {
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
 
-//             async authorize(credentials) {
-//                 if (credentials == null) return null;
+        //Find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
 
-//                 //Find user in database
-//                 const user = await prisma.user.findFirst({
-//                     where: {
-//                         email: credentials.email as string,
-//                     }
-//                 });
+        //check if user exist and password matches
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
 
-//                 //Checl is user exists of if password is correct
-//                 if (user && user.password) {
-//                     const isMatch = compareSync(credentials.password as string, user.password);
+          //If password is correct, return to user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        //if not exist or password unmatched return null
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      //set user id from token
+      session.user.id = token.sub;
 
-//                     // If user exists and password matches, return user
-//                     if (isMatch) {
-//                         return {
-//                             id: user.id,
-//                             name: user.name,
-//                             email: user.email,
-//                             role: user.role
-//                         }
-//                     }
-//                 }
-//                 // If user not found or password doesn't match, return null
-//                 return null;
-//             },
-//         }),
-//     ],
-//     callbacks: {
-//         async session({ session, user, trigger, token }: any) {
-//             //set the user id from the token
-//             session.user.id = token.sub;
+      //If there is an update, set the user name
+      if (trigger === 'update') {
+        session.user.name = user.name;
+      }
 
-//             // If there is an update, set the user name
-//             if (trigger === 'update'){
-//                 session.user.name = user.name;
-//             }
-//             return session;
-//         },
-//     },
-// };
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
 
-// export const { handlers, auth, signIn, signOut } = NextAuth(config);
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
