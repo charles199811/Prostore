013cc86bd175a7ff6c9c0252abea0ab0f8680f63
Charles Rodrigo <charlesrodrio@gmail.com>
2025-08-01 15:41:57 +0100

    set validators and user actions for the sign up form

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { date, z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
 const currency = z
@@ -26,3 +26,18 @@ export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 charachters"),
 });
+
+//Schema for signUp users in
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name should be at least 3 charachters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 charachters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm password must be at least 6 charachters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
