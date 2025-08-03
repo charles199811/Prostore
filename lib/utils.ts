500ddc641a34642ba98931b536b512beaa5e1f38
Charles Rodrigo <charlesrodrio@gmail.com>
2025-08-03 15:52:03 +0100

    Error handling for sign up for and modified the utills for format errors

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,22 +1,47 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 // Convert prisma object in to a regular JS object
-export function convertToPlanObject<T> (value: T): T {
+export function convertToPlanObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
 
 // Format number with decimal places
-export function formatNumberWithDecimal(num:number): string {
-  const [int, decimal] = num.toString().split('.');
-  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
 
 //convert prisma object in to regular JS object
-export function converToPlainObject<T>(value: T): T{
+export function converToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
+}
+
+//Format errors
+/// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function formatError(error: any) {
+  if (error.name === "ZodError") {
+    //Handle zod error
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    //Handle prisma error
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  } else {
+    //Handle other errors
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
 }
