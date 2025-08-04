81b7a0af6ee961624c74567e4418f5ab577197d5
Charles Rodrigo <charlesrodrio@gmail.com>
2025-08-04 16:16:01 +0100

    Customize token with JWT call back

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { use } from "react";
 
 export const config = {
   pages: {
@@ -57,6 +58,8 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       //set user id from token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       //If there is an update, set the user name
       if (trigger === "update") {
@@ -64,6 +67,25 @@ export const config = {
       }
 
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      //asign user field to token
+      if (user) {
+        token.role = user.role;
+
+        //If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          //Update database to reflect the tokec name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      return token;
     },
   },
 } satisfies NextAuthConfig;
