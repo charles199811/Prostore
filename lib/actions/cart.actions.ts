29e65f9ff8658f0a6a84a63344b06282e617acd3
Charles Rodrigo <charlesrodrio@gmail.com>
2025-08-11 17:48:55 +0100

    Remove cart action

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -8,6 +8,7 @@ import { prisma } from "@/db/prisma";
 import { cartItemSchema, insertCartSchema } from "../validators";
 import { revalidatePath } from "next/cache";
 import { Prisma } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 //Calculate cart price
 const calcPrice = (items: CartItem[]) => {
@@ -150,6 +151,53 @@ export async function getMyCart() {
 //Remove Item from cart
 export async function removeItemFromCart(productId: string) {
   try {
+    //Check for cart cookie
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    if (!sessionCartId) throw new Error("Cart session not found");
+
+    //Get product
+    const product = await prisma.product.findFirst({
+      where: { id: productId },
+    });
+    if (!product) throw new Error("Product not found");
+
+    //Get user cart
+    const cart = await getMyCart();
+    if (!cart) throw new Error("Cart not found");
+
+    //Check for item
+    const exist = (cart.items as CartItem[]).find(
+      (x) => x.productId === productId
+    );
+    if (!exist) throw new Error("Item not found");
+
+    //Check if only one in qty
+    if (exist.qty === 1) {
+      //Remove from cart
+      cart.items = (cart.items as CartItem[]).filter(
+        (x) => x.productId !== exist.productId
+      );
+    } else {
+      //Decrease the qty
+      (cart.items as CartItem[]).find((x) => x.productId === productId)!.qty =
+        exist.qty - 1;
+    }
+
+    //Update cart in database
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: cart.items as Prisma.CartUpdateitemsInput[],
+        ...calcPrice(cart.items as CartItem[]),
+      },
+    });
+
+    revalidatePath(`/product/${product.slug}`);
+
+    return {
+      success: true,
+      message: `${product.name} was removed from cart`,
+    };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
