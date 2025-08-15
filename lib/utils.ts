2c63dfc990772f3c2690fa4b287ccec83266ed38
Charles Rodrigo <charlesrodrio@gmail.com>
2025-08-15 14:29:39 +0100

    add plus-minus buttons, price, and subtotal to table

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,8 +16,6 @@ export function formatNumberWithDecimal(num: number): string {
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
 
-
-
 //Format errors
 /// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function formatError(error: any) {
@@ -51,5 +49,22 @@ export function round2(value: number | string) {
     return Math.round(((Number(value) + Number.EPSILON) * 100) / 100);
   } else {
     throw new Error("Value is not a number or string");
+  }
+}
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+
+//Format currency using the formatter above
+export function formatCurreny(amount: number | string | null) {
+  if (typeof amount === "number") {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === "string") {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return NaN;
   }
 }
