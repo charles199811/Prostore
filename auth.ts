4f4ceed89b2a8c81b64e923b2339f42e4f15dbc9
Charles Rodrigo <charlesrodrio@gmail.com>
2025-08-28 16:46:10 +0100

    Update the Auth.ts delete current cart and assign new cart

--- auth.ts
+++ auth.ts
@@ -83,7 +83,31 @@ export const config = {
             data: { name: token.name },
           });
         }
+
+        if (trigger === "signIn" || trigger === "signUp") {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              //Delete current cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+              //Asign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id},
+                data: {userId: user.id}
+              })
+            }
+          }
+        }
       }
+
       return token;
     },
 
