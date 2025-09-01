c034b1a92ff503872cb79ab839843e268e54f26a
Charles Rodrigo <charlesrodrio@gmail.com>
2025-09-01 14:32:18 +0100

    Set the constant variable

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -25,3 +25,9 @@ export const shippingAddressDefultValues = {
   postalCode: "",
   country: "",
 };
+
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+  ? process.env.PAYMENT_METHODS.split(", ")
+  : ["PayPal, Stripe, CashOnDelivery"];
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
