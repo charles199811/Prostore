044f37f076d545d613d1bc257d4a00e14f3facd5
Charles Rodrigo <charlesrodrio@gmail.com>
2025-09-03 15:54:17 +0100

    Ended up payment method form

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -2,22 +2,26 @@ import { Metadata } from "next";
 import { auth } from "@/auth";
 import { getUserById } from "@/lib/actions/user.actions";
 import PaymentMethodForm from "./payment-method-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
 
 export const metadata: Metadata = {
   title: "Select Payment Method",
 };
 
 const PaymentMethodPage = async () => {
-    const session = await auth();
-    const userId = session?.user?.id;
+  const session = await auth();
+  const userId = session?.user?.id;
 
-    if(!userId) throw new Error("User not found");
+  if (!userId) throw new Error("User not found");
 
-    const user = await getUserById(userId);
+  const user = await getUserById(userId);
 
-  return <>
-    <PaymentMethodForm preferredPaymentMethod ={ user.paymentMethod} />
-  </>;
+  return (
+    <>
+      <CheckoutSteps current={2} />
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+    </>
+  );
 };
 
 export default PaymentMethodPage;
