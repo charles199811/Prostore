4fccc67ab32171dcea14ae03a15c8e7d0137e16f
Charles Rodrigo <charlesrodrio@gmail.com>
2025-09-10 14:16:07 +0100

    create get orderby id action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,14 +1,13 @@
 "use server";
 
 import { isRedirectError } from "next/dist/client/components/redirect-error";
-import { formatError } from "../utils";
+import { converToPlainObject, formatError } from "../utils";
 import { auth } from "@/auth";
 import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 import { prisma } from "@/db/prisma";
 import { CartItem } from "@/types";
-
 
 // Create order and create the order items
 export async function createOrder() {
@@ -99,4 +98,19 @@ export async function createOrder() {
     if (isRedirectError(error)) throw error;
     return { success: false, message: formatError(error) };
   }
+}
+
+//Get order by id
+export async function getOrderById(orderId: string) {
+  const data = await prisma.order.findFirst({
+    where: {
+      id: orderId,
+    },
+    include: {
+      orderitems: true,
+      user: { select: { name: true, email: true } },
+    },
+  });
+
+  return converToPlainObject(data)
 }
