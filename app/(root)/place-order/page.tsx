85b18b3abd49d9a7b5b52773a7ae8d18d59ded86
Charles Rodrigo <charlesrodrio@gmail.com>
2025-09-12 18:04:29 +0100

    Update the Order Details table

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -114,7 +114,8 @@ const PlaceOrderPage = async () => {
             </CardContent>
           </Card>
         </div>
-        <Card>
+       <div>
+         <Card>
           <CardContent className="p-4 gap-4 space-y-4">
             <div className="flex justify-between">
               <div>Items</div>
@@ -138,6 +139,7 @@ const PlaceOrderPage = async () => {
             <PlaceOrderForm/>
           </CardContent>
         </Card>
+       </div>
       </div>
     </>
   );
