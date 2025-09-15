a38e060a5cdfd57b372e3a1862c0388da3849077
Charles Rodrigo <charlesrodrio@gmail.com>
2025-09-15 15:07:39 +0100

    create the generate paypal access token function

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -9,5 +9,20 @@ async function generateAccessToken() {
     `${PAYPAL_CLIENT_ID}: ${PAYPAL_APP_SECRET}`
   ).toString("base64");
 
-  const response = await fetch(`${base}/v1/oauth2/token`)
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: "POST",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  if (response.ok) {
+    const jsonData = await response.json();
+    return jsonData.access_token;
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
 }
