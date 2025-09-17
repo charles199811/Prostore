4f9b42c3a5634794d35a551c4f376dd72668d724
Charles Rodrigo <charlesrodrio@gmail.com>
2025-09-17 14:14:19 +0100

    test the paypal sandbox tonken generate

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -5,9 +5,9 @@ export const paypal = {};
 //Generate paypal access token
 async function generateAccessToken() {
   const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
-  const auth = Buffer.from(
-    `${PAYPAL_CLIENT_ID}: ${PAYPAL_APP_SECRET}`
-  ).toString("base64");
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    "base64"
+  );
 
   const response = await fetch(`${base}/v1/oauth2/token`, {
     method: "POST",
@@ -26,3 +26,5 @@ async function generateAccessToken() {
     throw new Error(errorMessage);
   }
 }
+
+export { generateAccessToken };
