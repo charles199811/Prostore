a5cb58628475f38588600afa575a116634aec178
Charles Rodrigo <charlesrodrio@gmail.com>
2025-10-08 18:22:48 +0100

    created chart for overview

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,9 +1,19 @@
 import { auth } from "@/auth";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { getOrderSummary } from "@/lib/actions/order.actions";
-import { formatCurreny, formatNumber } from "@/lib/utils";
-import { BadgeDollarSign, Barcode, CreditCard, User, Users } from "lucide-react";
+import { formatCurreny, formatDateTime, formatNumber } from "@/lib/utils";
+import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
+import Link from "next/link";
+import Charts from "./charts";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -49,7 +59,7 @@ const AdminOverviewPage = async () => {
         <Card>
           <CardHeader className="felx flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Customers</CardTitle>
-            <Users/>
+            <Users />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">
@@ -57,16 +67,65 @@ const AdminOverviewPage = async () => {
             </div>
           </CardContent>
         </Card>
-         <Card>
+        <Card>
           <CardHeader className="felx flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Products</CardTitle>
-            <Barcode/>
+            <Barcode />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">
               {formatNumber(summary.productsCount)}
             </div>
           </CardContent>
+        </Card>
+      </div>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+            <CardContent>
+              <Charts
+                data={{
+                  salesData: summary.salesData,
+                }}
+              />
+            </CardContent>
+          </CardHeader>
+        </Card>
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Recent Sales</CardTitle>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>BUYER</TableHead>
+                    <TableHead>DATE</TableHead>
+                    <TableHead>TOTAL</TableHead>
+                    <TableHead>ACTIONS</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {summary.latestSales.map((order) => (
+                    <TableRow key={order.id}>
+                      <TableCell>
+                        {order?.user?.name ? order.user.name : "Deleted User"}
+                      </TableCell>
+                      <TableCell>
+                        {formatDateTime(order.createdAt).dateOnly}
+                      </TableCell>
+                      <TableCell>{formatCurreny(order.totalPrice)}</TableCell>
+                      <TableCell>
+                        <Link href={`/order/${order.id}`}>
+                          <span className="px-2">Details</span>
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </CardHeader>
         </Card>
       </div>
     </div>
