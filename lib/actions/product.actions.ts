4e420994a494eebace476c354e5b0b515adfd411
Charles Rodrigo <charlesrodrio@gmail.com>
2025-10-16 16:35:18 +0100

    Display products on admin page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,23 +1,46 @@
-'use server';
+"use server";
 
-import { prisma } from '@/db/prisma';
-import { converToPlainObject } from '../utils';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { prisma } from "@/db/prisma";
+import { converToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 
 //get latest products
 export async function getLatestProduct() {
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
 
-    const data = await prisma.product.findMany({
-        take: LATEST_PRODUCTS_LIMIT,
-        orderBy: {createdAt: 'desc'}
-    });
-    
-    return converToPlainObject(data);
+  return converToPlainObject(data);
 }
 
 //get single product by slug
 export async function getProductBySlug(slug: string) {
-    return await prisma.product.findFirst({
-        where: {slug: slug},
-    });
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
+
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
