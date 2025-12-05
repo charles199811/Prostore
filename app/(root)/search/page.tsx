34f25005231d961cb002d62e8640189ca0f91dca
Charles Rodrigo <charlesrodrio@gmail.com>
2025-12-05 16:51:25 +0000

    update search page in admin

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,11 +1,38 @@
 import ProductCard from "@/components/shared/product/product-card";
+import { Button } from "@/components/ui/button";
 import { CardContent } from "@/components/ui/card";
 import {
   getAllProducts,
   getAllCategories,
 } from "@/lib/actions/product.actions";
 import { PAGE_SIZE } from "@/lib/constants";
+import { Value } from "@radix-ui/react-select";
 import Link from "next/link";
+
+const prices = [
+  {
+    name: "$1 to $50",
+    Value: "1-50",
+  },
+  {
+    name: "$51 to $100",
+    Value: "51-100",
+  },
+  {
+    name: "$101 to $200",
+    Value: "101-200",
+  },
+  {
+    name: "$201 to $500",
+    Value: "201-500",
+  },
+  {
+    name: "$501 to $1000",
+    Value: "501-1000",
+  },
+];
+
+const ratings = [4, 3, 2, 1];
 
 const SearchPage = async (props: {
   searchParams: Promise<{
@@ -91,12 +118,82 @@ const SearchPage = async (props: {
             ))}
           </ul>
         </div>
+
+        {/*Price Links */}
+        <div className="text-xl mb-2 mt-4">Price</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${price === "all" && "font-bold"}`}
+                href={getFilterUrl({ p: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {prices.map((p) => (
+              <li key={p.Value}>
+                <Link
+                  className={`${price === p.Value && "font-bold"}`}
+                  href={getFilterUrl({ p: p.Value })}
+                >
+                  {p.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/*Rating Links */}
+        <div className="text-xl mb-2 mt-3">Customer Ratings</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${rating === "all" && "font-bold"}`}
+                href={getFilterUrl({ r: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {ratings.map((r) => (
+              <li key={r}>
+                <Link
+                  className={`${rating === r.toString() && "font-bold"}`}
+                  href={getFilterUrl({ r: `${r}` })}
+                >
+                  {`${r} stars & up`}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="md:col-span-4 space-y-4">
-        {products.data.length == 0 && <div>No products found</div>} :
-        {products.data.map((product) => (
-          <ProductCard key={product.id} product={product}></ProductCard>
-        ))}
+        <div className="flex-between flext-col md:flex-row my-4">
+          <div className="flex items-center">
+            {q !== "all" && q !== "" && "Query: " + q}
+            {category !== "all" && category !== "" && "Category: " + category}
+            {price !== "all" && " Price: " + price}
+            {rating !== "all" && " Rating: " + rating + " & up"}
+            &nbsp;
+            {(q !== "all" && q !== "") ||
+            (category !== "all" && category !== "") ||
+            rating !== "all" ||
+            price !== "all" ? (
+              <Button variant={"link"} asChild>
+                <Link href="/search">Clear</Link>
+              </Button>
+            ) : null}
+          </div>
+          <div>{/* Sort */}</div>
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {products.data.length == 0 && <div>No products found</div>}
+          {products.data.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
